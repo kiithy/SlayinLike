04a7655233cb7fc642764ddadbf4c2842820a79d
kiithy <thekeithchua@gmail.com>
2025-02-27 03:50:38 +0800

    add functionality to move to next scene upon statue touch

--- Assets/Statue.cs
+++ Assets/Statue.cs
@@ -1,10 +1,13 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Statue : MonoBehaviour
 {
     public AudioSource statueAudioSource;
     public AudioClip groundImpactSound;
     private bool hasPlayedSound = false;
+    public string nextSceneName;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -15,6 +18,14 @@ public class Statue : MonoBehaviour
                 statueAudioSource.PlayOneShot(groundImpactSound);
                 hasPlayedSound = true;  // Prevent sound from playing multiple times
             }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync(nextSceneName, LoadSceneMode.Single);
         }
     }
 }
